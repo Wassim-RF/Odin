063aee59a1b6f8ce8c22628ecbb2ba79f32c52f2
wassim-rifi-dev <wassimrifi.dev@gmail.com>
2026-02-03 17:08:36 +0100

    feat: ajoute du check de login

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -7,19 +7,18 @@ use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
 
-class CheckAccountStatus
+class Authenticate
 {
     /**
      * Handle an incoming request.
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next): Response
     {
-        if (Auth::check() && !Auth::user()->is_active) {
-            return redirect('/login')->with('error', 'Votre compte est désactivé. Veuillez contacter l`administrateur.');
+        if (!Auth::check()) {
+            return redirect('/login');
         }
-
         return $next($request);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Requests\LoginRequest;
 use App\Http\Middleware\CheckAccountStatus;
+use App\Http\Middleware\Authenticate;
 use App\Http\Controllers\userController;
 use App\Http\Controllers\authController;
 use App\Http\Controllers\CategoriesController;
@@ -10,16 +11,29 @@ use App\Http\Controllers\LinksController;
 use App\Http\Controllers\TagsController;
 
 
-//get
+/* Public */
+//get 
 Route::get('/', [userController::class , "index"]);
 Route::get('/login' , [authController::class , "showLogin"]);
 Route::get('/register' , [authController::class , "showRegister"]);
-Route::get('/home' , [userController::class , 'showHome'])->middleware(CheckAccountStatus::class);
 
 //post
 Route::post('/login' , [authController::class , "login"])->name('auth.login');
 Route::post('/register' , [authController::class , "register"])->name('auth.register');
-Route::post('/logout' , [authController::class , "logout"])->name('auth.logout');
-Route::post('/addCategorie' , [CategoriesController::class , 'store'])->name('create.categorie');
-Route::post('/addLink' , [LinksController::class , 'store'])->name('create.link');
-Route::post('/addTag' , [TagsController::class , 'store'])->name('create.tag');
+
+
+/* Private */ 
+Route::middleware(Authenticate::class)->group(function () {
+    //get
+    Route::get('/home' , [userController::class , 'showHome'])->middleware(CheckAccountStatus::class);
+
+    //post
+    Route::post('/logout' , [authController::class , "logout"])->name('auth.logout');
+    Route::post('/addCategorie' , [CategoriesController::class , 'store'])->name('create.categorie');
+    Route::post('/addLink' , [LinksController::class , 'store'])->name('create.link');
+    Route::post('/addTag' , [TagsController::class , 'store'])->name('create.tag');
+
+    //put
+
+    //delete
+});
